0d0135190852a567cfe38e0cdd8c68ce36cd9d49
nishchals961 <nishchals96@gmail.com>
2021-02-02 09:28:34 +0530

    Add files via upload

--- src/servlets/EditAccountant.java
+++ src/servlets/EditAccountant.java
@@ -15,6 +15,11 @@ import dao.AccountantDao;
 import dao.StudentDao;
 @WebServlet("/EditAccountant")
 public class EditAccountant extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
